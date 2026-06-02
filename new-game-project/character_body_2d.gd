7d50d1c3dad17ad0e0a06555a95eb8d98f02024e
damfle47 <damfle47@kingshigh.school.nz>
2026-06-03 10:59:48 +1200

    blah blah blah

--- new-game-project/character_body_2d.gd
+++ new-game-project/character_body_2d.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var timer = $Timer 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -21,5 +22,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("ui_right"):
+		timer.start()
